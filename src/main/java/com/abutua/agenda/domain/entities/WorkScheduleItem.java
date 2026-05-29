a5ee116b66342bc93fd29418c6d42de8ede17c87
MichaelDouglasLima <michael.ignis2022@gmail.com>
2026-05-29 12:26:04 -0300

    desafio status agendamento

--- src/main/java/com/abutua/agenda/domain/entities/WorkScheduleItem.java
+++ src/main/java/com/abutua/agenda/domain/entities/WorkScheduleItem.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +24,10 @@ public class WorkScheduleItem implements Serializable {
   private LocalTime endTime;
   private Integer slots;
   private Integer slotSize;
+
+  @ManyToOne
+  @JoinColumn(name = "professional_id")
+  private Professional professional;
 
   public WorkScheduleItem() {
 
@@ -82,6 +88,14 @@ public class WorkScheduleItem implements Serializable {
 
   public void setSlotSize(Integer slotSize) {
     this.slotSize = slotSize;
+  }
+
+  public Professional getProfessional() {
+    return professional;
+  }
+
+  public void setProfessional(Professional professional) {
+    this.professional = professional;
   }
 
   @Override
--- src/main/java/com/abutua/agenda/domain/services/usecases/read/SearchProfessionalAvailabiltyTimesUseCase.java
+++ src/main/java/com/abutua/agenda/domain/services/usecases/read/SearchProfessionalAvailabiltyTimesUseCase.java
@@ -2,27 +2,77 @@ package com.abutua.agenda.domain.services.usecases.read;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.abutua.agenda.domain.entities.Appointment;
+import com.abutua.agenda.domain.entities.AppointmentStatus;
 import com.abutua.agenda.domain.entities.Professional;
+import com.abutua.agenda.domain.entities.WorkScheduleItem;
 import com.abutua.agenda.domain.models.TimeSlot;
+import com.abutua.agenda.domain.repositories.AppointmentRepository;
+import com.abutua.agenda.domain.repositories.WorkScheduleItemRepository;
 
 @Service
 public class SearchProfessionalAvailabiltyTimesUseCase {
 
+  @Autowired
+  private WorkScheduleItemRepository workScheduleItemRepository;
+
+  @Autowired
+  private AppointmentRepository appointmentRepository;
+
   public List<TimeSlot> executeUseCase(Professional professional, LocalDate date) {
 
-    // TODO - create real execute case
+    List<TimeSlot> timeSlots = new ArrayList<TimeSlot>();
+    List<WorkScheduleItem> workScheduleItems = getWorkScheduleItems(professional, date);
+    List<Appointment> appointments = getAppointments(professional, date);
 
-    TimeSlot t1 = new TimeSlot(LocalTime.parse("08:00:00"), LocalTime.parse("08:30:00"), true);
-    TimeSlot t2 = new TimeSlot(LocalTime.parse("08:30:00"), LocalTime.parse("09:00:00"), true);
-    TimeSlot t3 = new TimeSlot(LocalTime.parse("09:00:00"), LocalTime.parse("09:30:00"), true);
-    TimeSlot t4 = new TimeSlot(LocalTime.parse("09:30:00"), LocalTime.parse("10:00:00"), true);
-    TimeSlot t5 = new TimeSlot(LocalTime.parse("10:00:00"), LocalTime.parse("10:30:00"), true);
+    for (var item : workScheduleItems) {
+      timeSlots.addAll(this.calculateTimeSlots(item, appointments));
+    }
 
-    return Arrays.asList(t1, t2, t3, t4, t5);
+    return timeSlots;
+  }
+
+  private List<TimeSlot> calculateTimeSlots(WorkScheduleItem item, List<Appointment> appointments) {
+    var startTime = item.getStartTime();
+    var slotSize = item.getSlotSize();
+    var slots = item.getSlots();
+
+    List<TimeSlot> timeSlots = new ArrayList<TimeSlot>();
+
+    for (int i = 0; i < slots; i++) {
+      var start = startTime.plusMinutes(i * slotSize);
+      var end = start.plusMinutes(slotSize);
+
+      boolean available = this.isTimeSlotAvailable(start, end, appointments);
+
+      TimeSlot timeSlot = new TimeSlot(start, end, available);
+
+      timeSlots.add(timeSlot);
+    }
+
+    return timeSlots;
+  }
+
+  private boolean isTimeSlotAvailable(LocalTime start, LocalTime end, List<Appointment> appointments) {
+    return appointments.stream().noneMatch(
+        a -> a.getStartTime().isBefore(end) && a.getEndTime().isAfter(start) && (a.getStatus() != AppointmentStatus.OPEN
+            && a.getStatus() != AppointmentStatus.ABSENT && a.getStatus() != AppointmentStatus.PRESENT));
+  }
+
+  private List<Appointment> getAppointments(Professional professional, LocalDate date) {
+    return this.appointmentRepository.findByProfessionalIdAndDate(professional.getId(), date);
+  }
+
+  private List<WorkScheduleItem> getWorkScheduleItems(Professional professional, LocalDate date) {
+    return this.workScheduleItemRepository
+        .getWorkScheduleFromProfessionalByDayOfWeekOrderByStartTime(professional,
+            date.getDayOfWeek());
   }
 }
